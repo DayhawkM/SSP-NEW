b2ceaf53b638fd19ff017c9ac8239d23c4479eb0
DayhawkM <158460444+DayhawkM@users.noreply.github.com>
2024-02-22 22:23:39 +0000

    week 3 commit 2

--- Week 3/index.js
+++ Week 3/index.js
@@ -7,6 +7,9 @@ const app = express();
 app.disable("x-powered-by");
 app.set("view engine", "ejs");
 
+app.use(express.static('public'));
+app.use("/favicon.ico", express.static("public\\assets\\ico\\SSP.ico"));
+
 app.listen(8080, () =>
 {
 console.log("Server listening...");
