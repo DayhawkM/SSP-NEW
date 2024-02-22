4f528e452dfc2a34e3ddc580b80562653aaee64c
DayhawkM <158460444+DayhawkM@users.noreply.github.com>
2024-02-22 22:53:46 +0000

    week 3 commit 2

--- Week 3/index.js
+++ Week 3/index.js
@@ -10,6 +10,32 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use("/favicon.ico", express.static("public\\assets\\ico\\SSP.ico"));
 
+app.get("/", (req, res) =>
+{
+res.render("index");
+});
+
+
+app.use((req, res, next) =>
+{
+res.status(404);
+res.format(
+{
+html: () =>
+{
+res.render("404", {url: req.protocol + "://" + req.hostname + req.originalUrl});
+},
+json: () =>
+{
+res.json({error: "Not found"});
+},
+default: () =>
+{
+res.type("txt").send("Not found");
+}
+});
+})
+
 app.listen(8080, () =>
 {
 console.log("Server listening...");
